13d0799a78abd09abca9a33ea1dfc30120af9b2a
ruotongyu <yuruotong@ceshiren.com>
2024-03-16 12:41:16 +0800

    修复参数显示的问题

--- actions/action_base.py
+++ actions/action_base.py
@@ -1,10 +1,10 @@
 from typing import Type
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QHBoxLayout, QLabel, QLineEdit, QWidget, QPushButton
+from PyQt6.QtWidgets import QHBoxLayout, QLabel, QLineEdit, QWidget, QPushButton, QVBoxLayout
 from langchain_core.tools import StructuredTool
 from pydantic import BaseModel
-from pages.include_action_ui import IncludeActionUi
+
 from utils.qt_util import QtUtil
 
 
@@ -38,24 +38,27 @@ class ActionBase:
     # 设置配置界面的布局
     def config_page_ui(self):
         self.__config_ui = QtUtil.load_ui("config_page.ui")
-        # 水平布局
-        h_box_layout = QHBoxLayout()
-        label = QLabel(self.__config_ui)
+
+        v_box_layout = QVBoxLayout()
         model_fields = self.args_schema.model_fields
         for field in model_fields:
+            # 水平布局
+            h_box_layout = QHBoxLayout()
+            label = QLabel(self.__config_ui)
             label.setText(model_fields[field].title)
             line_edit = QLineEdit(self.__config_ui)
             h_box_layout.addWidget(label)
             h_box_layout.addWidget(line_edit)
             # 将输入内容填入参数列表
             self.__ui_name_and_line_edit[field] = line_edit
+            v_box_layout.addLayout(h_box_layout)
         save_button: QPushButton = self.__config_ui.saveButton
         save_button.clicked.connect(self.__save_button_clicked)
 
         cancel_button: QPushButton = self.__config_ui.cancelButton
         cancel_button.clicked.connect(self.__cancel_button_clicked)
         container_widget = QWidget(self.__config_ui)
-        container_widget.setLayout(h_box_layout)
+        container_widget.setLayout(v_box_layout)
         self.__config_ui.config_list.addWidget(container_widget)
 
     def __cancel_button_clicked(self):
