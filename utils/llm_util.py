9aadc797f76161b374202840924746d40ffb854e
greenflute <greenflute@hotmail.com>
2024-03-21 23:42:11 +0100

    lazy openai init, fixed print of none type, added official pypi sources

--- utils/llm_util.py
+++ utils/llm_util.py
@@ -1,5 +1,3 @@
-from langchain_openai import ChatOpenAI
-
 from utils.config import Config
 
 
@@ -9,7 +7,9 @@ class LLMUtil:
         self.config = Config()
         self.api_key = self.config.OPEN_AI.get("api_key")
         self.base_url = self.config.OPEN_AI.get("api_url")
+        self.model_name = self.config.OPEN_AI.get("model_name", "gpt-3.5-turbo")
 
     def llm(self):
-        return ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=self.api_key,
+        from langchain_openai import ChatOpenAI
+        return ChatOpenAI(temperature=0, model_name=self.model_name, openai_api_key=self.api_key,
                           openai_api_base=self.base_url)
