92dd32c12152e4fb41dfab63a34d5a5c95ede4f3
yuruo <yuruotong1@163.com>
2024-04-10 21:04:06 +0800

    更新资源文件

--- package.bat
+++ package.bat
@@ -1,1 +1,3 @@
-pyinstaller -D -w main.py --add-data=pages/*.ui:pages
+pyinstaller -D -w main.py --add-data=pages/*.ui:pages --add-data=source/*:source
+move dist/main/_internal/pages dist/main/pages
+move dist/main/_internal/source dist/main/source
