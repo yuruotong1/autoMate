b27bc85f088673dcb56ed49da304ce82382459b4
yuruo <yuruotong1@163.com>
2024-05-06 10:41:13 +0800

    更新 readme 资源文件

--- package.bat
+++ package.bat
@@ -1,4 +1,12 @@
-pyinstaller -D -w main.py --add-data=pages/*.ui:pages  --add-data=source/*:source
-move dist/main/_internal/pages dist/main/pages
-move dist/main/_internal/source dist/main/source
-copy "config_tmp.yaml" "dist/main/config_tmp.yaml"
+echo Y | pyinstaller -D -w --add-data=pages/*.ui:pages  --add-data=source/*:source -i source/logo.ico --name=autoMate main.py 
+move dist/autoMate/_internal/pages dist/autoMate/pages
+move dist/autoMate/_internal/source dist/autoMate/source
+copy "config_tmp.yaml" "dist/autoMate/config.yaml"
+
+@REM cd dist/main
+@REM ren "main.exe" "autoMate.exe"
+cd dist
+del "autoMate.zip"
+powershell Compress-Archive -Path autoMate -DestinationPath autoMate.zip
+cd ..
+
