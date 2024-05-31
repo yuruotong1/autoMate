400b5e30d26633efba761f09dddfefaf15fd1502
yuruo <yuruotong1@163.com>
2024-05-31 10:18:04 +0800

    📝 添加(agent/prompt.py)：添加了关于对齐需求的提示文本 ✨ 引入(agent/prompt.py)：引入了对齐需求的AgentRequireAlignmentAgent

---
+++ agent/require_alignment_agent.py
@@ -0,0 +1,27 @@
+from actions.action_util import ActionUtil
+from agent.prompt import system_prompt
+from utils.llm_util import LLM_Util
+
+# 需求对齐Agent，负责与用户对齐需求
+class RequireAlignmentAgent:
+    def __init__(self):
+        action_descriptions = ""
+        for action_class in ActionUtil.get_actions():
+            action = action_class()
+            action_descriptions += action.package_actions_description() + "\n"
+        self.messages = [{"content": system_prompt.substitute(python_code=action_descriptions), "role": "system"}]
+
+    def run(self, question):
+        self.messages.append({"content": question, "role": "user"})
+        res = LLM_Util().invoke(self.messages)
+        self.messages.append({"content": res["content"], "role": "assistant"})
+        print(self.messages)
+        # self.messages.append({"content": res, "role": "assistant"})
+        return res
+
+        
+
+
+
+
+
