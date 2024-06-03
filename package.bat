91705678e100819a5bed528e69a57952fad42095
yuruo <yuruotong1@163.com>
2024-06-03 18:39:35 +0800

    🔧 更新(package.bat)：更新package.bat脚本以创建内部文件夹_self_utils并复制配置文件   📝 更新(package.bat)：注释掉不再需要的移动和重命名操作

--- package.bat
+++ package.bat
@@ -1,10 +1,9 @@
 echo Y | pyinstaller -D -w --add-data=pages/*.ui:pages  --add-data=source/*:source -i source/logo.ico  --hidden-import=tiktoken_ext.openai_public --hidden-import=tiktoken_ext --name=autoMate  main.py 
-move dist/autoMate/_internal/pages dist/autoMate/pages
-move dist/autoMate/_internal/source dist/autoMate/source
-copy "config_tmp.yaml" "dist/autoMate/config.yaml"
+mkdir dist/autoMate/_internal/self_utils
+@REM move dist/autoMate/_internal/pages dist/autoMate/pages
+@REM move dist/autoMate/_internal/source dist/autoMate/source
+copy "config_tmp.yaml" "dist/autoMate/_internal/config.yaml"
 
-@REM cd dist/main
-@REM ren "main.exe" "autoMate.exe"
 cd dist
 del "autoMate.zip"
 powershell Compress-Archive -Path autoMate -DestinationPath autoMate.zip
