dce32a7e3129064717d6990e24875344f382251c
yuruo <yuruotong1@163.com>
2024-06-13 16:12:10 +0800

    ✨ 新增(ui)：重构了Config页面，将原有的Config.tsx文件删除，新建了Config/index.tsx和Config/styles.module.scss文件，并增加了新的布局样式。同时，将原本的Home.tsx文件重命名为Home/index.tsx。

---
+++ ui/autoMate/src/renderer/src/pages/Config/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.scss"
+export default function Config(){
+    return (
+      <main className={styles.container}>
+        <div> </div>
+        <div> </div>
+        <div className={styles.category}> </div>
+        <div className={styles.nav}> </div>
+        <div className={styles.content}> </div>
+      </main>
+    )
+  }
+  
+
+  
