a30ef66241ba318acc0bb2b929b88273455bead7
ruotongyu <yuruotong@ceshiren.com>
2024-06-17 22:44:45 +0800

    ✨ ui(autoMate): 添加选中样式以及自动跳转至第一个内容页   📝 ui(autoMate): 引入react的useEffect和useNavigate功能

--- ui/autoMate/src/renderer/src/pages/ContentList/index.tsx
+++ ui/autoMate/src/renderer/src/pages/ContentList/index.tsx
@@ -1,8 +1,15 @@
-import { NavLink, Outlet, useLoaderData } from "react-router-dom"
+import { NavLink, Outlet, useLoaderData, useNavigate } from "react-router-dom"
 import "./content.scss"
+import { useEffect } from "react"
 
 export const ContentList = () => {
     const contentList = useLoaderData() as ContentType[]
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (contentList) {
+            navigate(`/config/category/contentList/${contentList[0].category_id}/content/${contentList[0].id}`)
+        }
+    }, [contentList])
     return (<main className="content-page">
         <div className="list">
             {contentList.map(content => (
