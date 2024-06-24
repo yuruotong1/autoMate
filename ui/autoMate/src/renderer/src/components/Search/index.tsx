9037a0a4a3e1a42526953d0b777262887c91967e
yuruo <yuruotong1@163.com>
2024-06-24 10:13:06 +0800

    ✨  新功能(Search/index.tsx)：更新搜索组件样式和交互逻辑   📝  文档(Search/index.tsx)：改变div和section元素，修复拼写错误并删除多余的class属性

--- ui/autoMate/src/renderer/src/components/Search/index.tsx
+++ ui/autoMate/src/renderer/src/components/Search/index.tsx
@@ -7,13 +7,13 @@ export default function Search(): JSX.Element {
   const {handleSearch} = useSearch()
   return (
     <main className="bg-slate-50 p-3 rounded-lg drag" >
-        <section className="bg-slate-200 p-3 rounded-lg flex items-center gap-1 nodrag">
+        <div className="bg-slate-200 p-3 rounded-l flex items-center gap-1 no-drag">
             <SettingOne 
               theme="outline"
               size="22"
               fill="#34495e"
               strokeWidth={4}
-              className="cursor-pointer z-10"
+              className="cursor-pointer"
               onClick={()=>window.api.openWindow('code')
               }
             />
@@ -23,8 +23,8 @@ export default function Search(): JSX.Element {
             onChange={handleSearch}
             autoFocus
             />
-        </section>
-        <section className="text-center text-slate-600 text-xs mt-2 nodrag">
+        </div>
+        <section className="text-center text-slate-600 text-xs mt-2 no-drag">
           autoMate 
           <span className="text-blue-600" onClick={()=>window.api.openWindow('config')}>配置信息</span>
         </section>
