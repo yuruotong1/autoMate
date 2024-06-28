461a50f62d910495c31510f62a253044b1242f70
yuruo <yuruotong1@163.com>
2024-06-28 16:13:21 +0800

    将 openai 调用逻辑抽离出来

--- src/renderer/src/hooks/useChat.ts
+++ src/renderer/src/hooks/useChat.ts
@@ -1,51 +1,19 @@
-import { createOpenAI } from "@ai-sdk/openai";
-import { StreamingTextResponse, streamText } from "ai";
-// import { createOpenAI } from '@ai-sdk/openai';
+import { requireAlignmentPrompt } from "./prompt";
+import useOpenai from "./useOpenai";
 
 export async function useChat(chat_messages: Array<any>){
-  const configType =  (await window.api.getConfig()) as ConfigType
-  const config = JSON.parse(configType.content) as ConfigDataType
   const messages = chat_messages.map((m) => {
     return {
       role: m.role, 
       content: m.content
     }
   })
-  const openai = createOpenAI({
-    apiKey: config.llm.apiKey,
-    baseURL: config.llm.baseURL,
-    compatibility: 'compatible'
+  // 添加 system 消息
+  messages.unshift({
+    role: 'system',
+    content: requireAlignmentPrompt()
   });
-  const stream = await streamText({
-    model: openai(config.llm.model),
-    messages: [...messages],
-  });
- // 获取 reader
- const reader = stream.textStream.getReader();
- const encoder = new TextEncoder();
-
-  const readableStream = new ReadableStream({
-    async start(controller) {
-      function push() {
-        reader
-          .read()
-          .then(({ done, value }) => {
-            if (done) {
-              controller.close();
-              return;
-            }
-            controller.enqueue(encoder.encode(value));
-            push();
-          })
-          .catch((err) => {
-            console.error('读取流中的数据时发生错误', err);
-            controller.error(err);
-          });
-      }
-      push();
-    },
-  });
-
-  return new Response(readableStream) // 支持流式和非流式
+  const response = await useOpenai(requireAlignmentPrompt(), messages)
+  return response
 }
 
---
+++ src/renderer/src/hooks/useOpenai.ts
@@ -0,0 +1,58 @@
+import { createOpenAI } from "@ai-sdk/openai"
+import { streamText } from "ai"
+
+export default async (systemPrompt: string, chatMessages: Array<Record<string, any>>, callback: (allContent: string) => void) => {
+    const configType = (await window.api.getConfig()) as ConfigType
+    const config = JSON.parse(configType.content) as ConfigDataType
+    const messages = chatMessages.map((m) => {
+        return {
+            role: m.role,
+            content: m.content
+        }
+    })
+    // 添加 system 消息
+    messages.unshift({
+        role: 'system',
+        content: systemPrompt
+    });
+    const openai = createOpenAI({
+        apiKey: config.llm.apiKey,
+        baseURL: config.llm.baseURL,
+        compatibility: 'compatible'
+    });
+
+
+    const stream = await streamText({
+        model: openai(config.llm.model),
+        messages: [...messages],
+    });
+    // 获取 reader
+    const reader = stream.textStream.getReader();
+    const encoder = new TextEncoder();
+
+    const readableStream = new ReadableStream({
+        async start(controller) {
+            function push() {
+                let res = ""
+                reader
+                    .read()
+                    .then(({ done, value }) => {
+                        if (done) {
+                            controller.close();
+                            callback(res)
+                            return;
+                        }
+                        res += value
+                        controller.enqueue(encoder.encode(value));
+                        push();
+                    })
+                    .catch((err) => {
+                        console.error('读取流中的数据时发生错误', err);
+                        controller.error(err);
+                    });
+            }
+            push();
+        },
+    });
+    return new Response(readableStream)
+}
