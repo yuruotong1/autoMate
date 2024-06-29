e7e16eea034bc0d014ca312becbafcc1104654ae
yuruo <yuruotong1@163.com>
2024-06-29 19:14:56 +0800

    传递代码到后端

--- app/src/renderer/src/pages/Interactive/index.tsx
+++ app/src/renderer/src/pages/Interactive/index.tsx
@@ -2,8 +2,10 @@ import "./chat-page.scss"
 import { Button } from 'antd';
 import Chat from "@renderer/components/Chat"
 import CodeEditor from "@renderer/components/Chat"
+import { localServerBaseUrl } from "@renderer/config";
+import { useStore } from "@renderer/store/useStore";
 export default function Interactive() {
-
+    const code = useStore(state => state.code)
     return (
         <div>
             <div className='chat-page'>
@@ -13,7 +15,12 @@ export default function Interactive() {
                 <CodeEditor />
                 <Button onClick={async () => {
                     try {
-                        const res = await fetch("http://127.0.0.1:5000/");
+                        const res = await fetch(localServerBaseUrl + "/", {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                code: code
+                            })
+                        });
                         const data = await res.json();
                         console.log(data);
                     } catch (error) {
--- server/route/code_executor.py
+++ server/route/code_executor.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
-
+from flask import request
 home_bp = Blueprint('home', __name__)
 
-@home_bp.route('/')
+@home_bp.route('/execute', methods=["POST"])
 def home():
+    code = request.get_json()
+    exec(code)
     return {"message": "你好"}
