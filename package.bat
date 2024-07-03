f4b892605860bd7e6566122b0fe85ea0adf48fb1
yuruo <yuruotong1@163.com>
2024-07-04 00:02:03 +0800

    更新打包脚本

--- package.bat
+++ package.bat
@@ -6,8 +6,16 @@ cd server
 call .\.venv\Scripts\activate
 call echo y | pyinstaller main.spec
 xcopy .\.venv\Lib\site-packages\litellm\*.json .\dist\autoMateServer\_internal\litellm\  /E /H /F /I /Y
+
+
+REM 设置要清空的目录路径
+set "target_dir=..\app\dist"
+REM 删除目标目录中的所有文件
+del /f /q "%target_dir%\*.*"
+REM 删除目标目录中的所有子目录
+for /d %%i in ("%target_dir%\*") do rmdir /s /q
+
 xcopy .\dist\autoMateServer\* ..\app\dist\win-unpacked\ /E /H /F /I /Y
 cd ..\app\dist
 REN win-unpacked autoMate
-del autoMate.zip
 powershell Compress-Archive -Path autoMate -DestinationPath autoMate.zip
