7de7ef711b9782d1fe5ed965bf415b7aaa6ab4c8
ruotongyu <yuruotong@ceshiren.com>
2024-07-09 19:21:13 +0800

    加入自动更新功能

--- package.bat
+++ package.bat
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 call echo y | pyinstaller main.spec
 xcopy .\.venv\Lib\site-packages\litellm\*.json .\dist\autoMateServer\_internal\litellm\  /E /H /F /I /Y
 
-cd app
+cd ..\app
 set "target_dir=.\dist"
 del /f /q "%target_dir%\*.*"
 for /d %%i in ("%target_dir%\*") do rmdir /s /q "%%i"
