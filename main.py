33dd99b1d85913e560af10d92548107719a96ae9
yuruo <yuruotong1@163.com>
2025-03-21 10:46:50 +0800

    delete gpu warning

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import os
 from util import download_weights
 os.environ["GRADIO_ANALYTICS_ENABLED"] = "False"
 def run():
-    if not torch.cuda.is_available():
-        print("Warning: GPU is not available, we will use CPU, the application may run slower!\nyou computer will very likely heat up!")
     download_weights.download() 
     app.run()
 
